f7d6706901eed8d31b221d9507a1dc818b779413
brunobarreira <brunobarreirasilva@gmail.com>
2023-01-02 17:15:42 -0300

    feat: use HttpRequestService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { SponsorComponentComponent } from './sponsor-component/sponsor-component.component';
+import { HttpRequestService } from './sponsor-component/http-request-service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent
+    TopBarComponent,
+    SponsorComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    HttpRequestService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
