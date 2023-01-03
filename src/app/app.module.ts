714cc23e8c69dde2f249642de07a104333c8750c
brunobarreira <brunobarreirasilva@gmail.com>
2023-01-03 00:31:26 -0300

    feat: use locale data methods

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,40 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID, DEFAULT_CURRENCY_CODE } from '@angular/core';
+import ptBr from '@angular/common/locales/pt';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { SponsorComponentComponent } from './sponsor-component/sponsor-component.component';
 import { HttpRequestService } from './sponsor-component/http-request-service';
+import { RecommendationComponentComponent } from './recommendation-component/recommendation-component.component';
+import { RecommendationBoxComponent } from './recommendation-box/recommendation-box.component';
 
+registerLocaleData(ptBr);
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
-    SponsorComponentComponent
+    SponsorComponentComponent,
+    RecommendationComponentComponent,
+    RecommendationBoxComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule
   ],
   providers: [
-    HttpRequestService
+    HttpRequestService,
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt',
+    },
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL',
+    },
   ],
   bootstrap: [AppComponent]
 })
